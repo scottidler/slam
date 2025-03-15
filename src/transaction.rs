a5db1c16292611e9e3113ab35879b330469193e8
Scott A. Idler <scott.a.idler@gmail.com>
2025-03-15 12:44:45 -0700

    cleaned up the naming in transactions.rs to my liking

--- src/transaction.rs
+++ src/transaction.rs
@@ -4,20 +4,20 @@ use eyre::Result;
 // Transaction Struct Definition
 //------------------------------------------------------------------------------
 /// A single rollback action: a boxed function that undoes a previously completed step.
-type RollbackAction = Box<dyn Fn() -> Result<()> + Send>;
+type Rollback = Box<dyn Fn() -> Result<()> + Send>;
 
 /// Transaction is a rollback stack for reversible operations.
 /// Each successful step can register a rollback closure. On error, all actions
 /// are invoked in reverse order.
 pub struct Transaction {
-    rollback_actions: Vec<RollbackAction>,
+    rollsbacks: Vec<Rollback>,
     committed: bool,
 }
 
 impl Transaction {
     pub fn new() -> Self {
         Transaction {
-            rollback_actions: Vec::new(),
+            rollsbacks: Vec::new(),
             committed: false,
         }
     }
@@ -27,13 +27,13 @@ impl Transaction {
     where
         F: Fn() -> Result<()> + Send + 'static,
     {
-        self.rollback_actions.push(Box::new(action));
+        self.rollsbacks.push(Box::new(action));
     }
 
     /// Executes rollback actions in reverse order. Each error is logged.
     pub fn rollback(&mut self) {
-        error!("An error occurred; initiating rollback of {} actions", self.rollback_actions.len());
-        while let Some(action) = self.rollback_actions.pop() {
+        error!("An error occurred; initiating rollback of {} actions", self.rollsbacks.len());
+        while let Some(action) = self.rollsbacks.pop() {
             if let Err(e) = action() {
                 error!("Rollback action failed: {:?}", e);
             } else {
@@ -45,7 +45,7 @@ impl Transaction {
     /// Marks the transaction as committed and clears the rollback stack.
     pub fn commit(&mut self) {
         self.committed = true;
-        self.rollback_actions.clear();
+        self.rollsbacks.clear();
     }
 }
 
